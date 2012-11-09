fa93576b3672a0332dd78fefce7f8ce0d68db724
Haisheng.W.WU <freizl@gmail.com>
2012-11-09 23:35:51 +0800

    hello script 5

--- hello-backbone/5.js
+++ hello-backbone/5.js
@@ -89,7 +89,7 @@ var ListView = Backbone.View.extend(
       render : function render () {
          var that = this;
          $(that.el).append('<button id="add">Add list item</button>');
-         $(that.el).append('<ul><li>Hello Backbone</li></ul>');
+         $(that.el).append('<ul><li>Hello Backbone - Script 5</li></ul>');
 
          /* in case collection is not empty
           * however it is empty at this example thus ignored.
