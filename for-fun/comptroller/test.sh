c418d85380aaf03fbdc6c61e20f78e1ce1798a51
Haisheng.W.WU <freizl@gmail.com>
2012-11-17 15:28:30 +0800

    a weekend fun

---
+++ for-fun/comptroller/test.sh
@@ -0,0 +1,5 @@
+node main.js ./datas/0.csv tofu_log
+node main.js ./datas/0.csv fancy_european_water
+node main.js ./datas/1.csv burger tofu_log
+node main.js ./datas/2.csv chef_salad wine_spritzer
+node main.js ./datas/3.csv fancy_european_water extreme_fajita
